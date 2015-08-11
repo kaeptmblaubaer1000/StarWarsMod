c37c665b47bfc3b1ffa7a2f70032ecd0a8929a6a
Colby Newman <colby.newman2000@gmail.com>
2015-08-11 09:47:59 -0400

    Versions and logos

--- src/main/java/com/parzi/starwarsmod/StarWarsMod.java
+++ src/main/java/com/parzi/starwarsmod/StarWarsMod.java
@@ -55,7 +55,7 @@ public class StarWarsMod
 {
 	/* Mod Details */
 	public static final String MODID = "starwarsmod";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.0";
 	public static Configuration config;
 
 	/* Beta Testing */
