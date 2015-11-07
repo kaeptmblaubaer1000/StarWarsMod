ae619b421e99f77ef64535f20088448868bc9444
Colby Newman <colby.newman2000@gmail.com>
2015-11-07 13:46:54 -0500

    Added 3D endor helmet

--- src/main/java/com/parzi/starwarsmod/armor/ArmorEndor.java
+++ src/main/java/com/parzi/starwarsmod/armor/ArmorEndor.java
@@ -1,14 +1,21 @@
 package com.parzi.starwarsmod.armor;
 
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 
 import com.parzi.starwarsmod.StarWarsMod;
+import com.parzi.starwarsmod.rendering.models.armor.ModelEndorHelmet;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ArmorEndor extends ItemArmor
 {
 	private String[] names = { "Helmet", "Chestplate", "Leggings", "Boots" };
+	private ModelEndorHelmet model = new ModelEndorHelmet();
 
 	public ArmorEndor(ItemArmor.ArmorMaterial par2EnumArmorMaterial, int par3, int par4)
 	{
@@ -19,9 +26,19 @@ public class ArmorEndor extends ItemArmor
 	}
 
 	@Override
+	@SideOnly(Side.CLIENT)
+	public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemstack, int armorSlot)
+	{
+		if (armorSlot == 0)
+			return model;
+		return null;
+	}
+
+	@Override
 	public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type)
 	{
-		if (stack.getItem() == StarWarsMod.endorHelmet || stack.getItem() == StarWarsMod.endorChest || stack.getItem() == StarWarsMod.endorBoots) return StarWarsMod.MODID + ":" + "textures/models/endorArmorLayer1.png";
+		if (stack.getItem() == StarWarsMod.endorHelmet) return StarWarsMod.MODID + ":" + "textures/models/endorHelmet.png";
+		if (stack.getItem() == StarWarsMod.endorChest || stack.getItem() == StarWarsMod.endorBoots) return StarWarsMod.MODID + ":" + "textures/models/endorArmorLayer1.png";
 		if (stack.getItem() == StarWarsMod.endorLegs) return StarWarsMod.MODID + ":" + "textures/models/endorArmorLayer2.png";
 		return "";
 	}
