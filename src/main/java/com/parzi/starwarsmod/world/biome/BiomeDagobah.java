c72df28010dd0a3feef49d3d852faed224dc002b
Colby Newman <colby.newman2000@gmail.com>
2015-11-14 17:12:33 -0500

    dagobah pt1

---
+++ src/main/java/com/parzi/starwarsmod/world/biome/BiomeDagobah.java
@@ -0,0 +1,109 @@
+package com.parzi.starwarsmod.world.biome;
+
+import java.util.Random;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenAbstractTree;
+import net.minecraft.world.gen.feature.WorldGenBigTree;
+import net.minecraft.world.gen.feature.WorldGenCanopyTree;
+import net.minecraft.world.gen.feature.WorldGenMegaJungle;
+import net.minecraft.world.gen.feature.WorldGenMegaPineTree;
+import net.minecraft.world.gen.feature.WorldGenSavannaTree;
+import net.minecraft.world.gen.feature.WorldGenTrees;
+
+import com.parzi.starwarsmod.StarWarsMod;
+import com.parzi.starwarsmod.world.gen.WorldGenMV;
+import com.parzi.starwarsmod.world.gen.WorldGenSuperTatooineHomestead;
+import com.parzi.starwarsmod.world.gen.WorldGenTatooineHomestead;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_00;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_01;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_02;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_03;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_04;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_05;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_06;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_07;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_08;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_10;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_11;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_12;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_13;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_14;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_15;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_16;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_17;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_18;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_20;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_21;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_22;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_23;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_24;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_25;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_26;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_27;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_28;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_30;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_31;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_32;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_33;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_34;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_35;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_36;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_37;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_38;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_40;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_41;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_42;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_43;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_44;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_45;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_46;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_47;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_48;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_50;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_51;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_52;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_53;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_54;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_55;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_56;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_57;
+import com.parzi.starwarsmod.world.gen.moseisley.big.ME_58;
+
+public class BiomeDagobah extends BiomeGenBase
+{
+	public BiomeDagobah(int par1)
+	{
+		super(par1);
+		this.heightVariation = 0.2F;
+		this.enableRain = true;
+		this.enableSnow = false;
+		this.spawnableMonsterList.clear();
+		this.spawnableCreatureList.clear();
+		this.spawnableCaveCreatureList.clear();
+		this.spawnableWaterCreatureList.clear();
+		this.setBiomeName("Dagobah");
+		this.topBlock = Blocks.grass;
+		this.fillerBlock = Blocks.dirt;
+		this.theBiomeDecorator.treesPerChunk = 4;
+	}
+
+	@Override
+	public void decorate(World par1World, Random par2Random, int chunkX, int chunkZ)
+	{
+		super.decorate(par1World, par2Random, chunkX, chunkZ);
+	}
+
+	@Override
+	public WorldGenAbstractTree func_150567_a(Random p_150567_1_)
+	{
+		return new WorldGenMegaJungle(false, 10, 20, 3, 3);
+	}
+}
+/*
+ * Location: C:\Users\Colby\Downloads\Parzi's Star Wars Mod
+ * v1.2.0-dev7.jar!\com\parzi\starwarsmod\world\biome\BiomeTatooine.class Java
+ * compiler version: 6 (50.0) JD-Core Version: 0.7.1
+ */
---
+++ src/main/java/com/parzi/starwarsmod/world/provider/WorldProviderDagobah.java
@@ -0,0 +1,113 @@
+package com.parzi.starwarsmod.world.provider;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.WorldProvider;
+import net.minecraft.world.biome.WorldChunkManagerHell;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.ChunkProviderGenerate;
+
+import com.parzi.starwarsmod.StarWarsMod;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class WorldProviderDagobah extends WorldProvider
+{
+	@Override
+	public boolean canCoordinateBeSpawn(int par1, int par2)
+	{
+		return false;
+	}
+
+	public boolean canRainSnowIce(Chunk chunk)
+	{
+		return true;
+	}
+
+	@Override
+	public boolean canRespawnHere()
+	{
+		return true;
+	}
+
+	@Override
+	public IChunkProvider createChunkGenerator()
+	{
+		return new ChunkProviderGenerateDagobah(this.worldObj, this.worldObj.getSeed(), false);
+	}
+
+	@Override
+	public int getAverageGroundLevel()
+	{
+		return 40;
+	}
+
+	@Override
+	public String getDimensionName()
+	{
+		return "Dagobah";
+	}
+
+	@Override
+	public ChunkCoordinates getEntrancePortalLocation()
+	{
+		return null;
+	}
+
+	/*
+	 * @Override
+	 *
+	 * @SideOnly(Side.CLIENT) public IRenderHandler getSkyRenderer() { return
+	 * new DrawTatooineSky(); }
+	 */
+
+	/**
+	 * Returns true if the given X,Z coordinate should show environmental fog.
+	 */
+	@SideOnly(Side.CLIENT)
+	public boolean doesXZShowFog(int p_76568_1_, int p_76568_2_)
+	{
+		return true;
+	}
+
+    @SideOnly(Side.CLIENT)
+    public Vec3 getSkyColor(Entity cameraEntity, float partialTicks)
+    {
+        return Vec3.createVectorHelper(0, 0.7, 0.51);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public Vec3 getFogColor(float p_76562_1_, float p_76562_2_)
+    {
+        return Vec3.createVectorHelper(0, 0.78, 0.59);
+    }
+
+	@Override
+	public ChunkCoordinates getSpawnPoint()
+	{
+		return new ChunkCoordinates(0, this.worldObj.getHeightValue(0, 0), 0);
+	}
+
+	@Override
+	public boolean isSurfaceWorld()
+	{
+		return true;
+	}
+
+	@Override
+	public void registerWorldChunkManager()
+	{
+		this.worldChunkMgr = new WorldChunkManagerHell(StarWarsMod.biomeDagobah, 0.0F);
+		this.dimensionId = StarWarsMod.dimDagobahId;
+	}
+}
+/*
+ * Location: C:\Users\Colby\Downloads\Parzi's Star Wars Mod
+ * v1.2.0-dev7.jar!\com\
+ * parzi\starwarsmod\world\provider\WorldProviderTatooine.class Java compiler
+ * version: 6 (50.0) JD-Core Version: 0.7.1
+ */
