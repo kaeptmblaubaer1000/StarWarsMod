19d01797a1556beed330888acf911ba82c33c249
Colby Newman <colby.newman2000@gmail.com>
2015-11-26 18:35:01 -0500

    dying things

--- src/main/java/com/parzi/starwarsmod/vehicles/VehicTIEInterceptor.java
+++ src/main/java/com/parzi/starwarsmod/vehicles/VehicTIEInterceptor.java
@@ -2,6 +2,7 @@ package com.parzi.starwarsmod.vehicles;
 
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 import com.parzi.starwarsmod.StarWarsMod;
@@ -20,7 +21,7 @@ public class VehicTIEInterceptor extends VehicleAirBase
 	@Override
 	public void dropFewItems(boolean par1, int par2)
 	{
-		this.dropItem(StarWarsMod.spawnTie, 1);
+		this.dropItem(StarWarsMod.spawnTieInterceptor, 1);
 	}
 
 	@Override
@@ -29,11 +30,30 @@ public class VehicTIEInterceptor extends VehicleAirBase
 		if (this.hasCustomNameTag()) return this.getCustomNameTag();
 		return "TIE/IN Interceptor";
 	}
+	
+	@Override
+	public void onDeath(DamageSource source)
+	{
+
+		for (int i = 0; i < 50; i++)
+		{
+			double motionX = rand.nextGaussian() * 0.02D;
+			double motionY = rand.nextGaussian() * 0.02D;
+			double motionZ = rand.nextGaussian() * 0.02D;
+			worldObj.spawnParticle("hugeexplode", posX + rand.nextFloat() * width * 2.0F - width, posY + 0.5D + rand.nextFloat() * height, posZ + rand.nextFloat() * width * 2.0F - width, motionX, motionY, motionZ);
+		}
+	}
 
 	@Override
 	public String getMovingSound()
 	{
 		return "vehicle.tie.move";
+	}
+	
+	@Override
+	public String getDeathSound()
+	{
+		return StarWarsMod.MODID + ":" + "vehicle.tie.die";
 	}
 }
 /*
