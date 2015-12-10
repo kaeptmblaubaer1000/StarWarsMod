6ce105b1355062bd84e6316eee4b3d0a1aa76f0a
Colby Newman <colby.newman2000@gmail.com>
2015-12-09 19:57:28 -0500

    bb8 bbDone

---
+++ src/main/java/com/parzi/starwarsmod/rendering/RenderDroidBb8.java
@@ -0,0 +1,29 @@
+package com.parzi.starwarsmod.rendering;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+
+import com.parzi.starwarsmod.StarWarsMod;
+
+public class RenderDroidBb8 extends RenderLiving
+{
+	public static ResourceLocation texture = new ResourceLocation(StarWarsMod.MODID, "textures/models/astromechBb8.png");
+	
+	public RenderDroidBb8(ModelBase par1ModelBase, float par2)
+	{
+		super(par1ModelBase, par2);
+	}
+
+	@Override
+	protected ResourceLocation getEntityTexture(Entity entity)
+	{
+		return texture;
+	}
+}
+/*
+ * Location: C:\Users\Colby\Downloads\Parzi's Star Wars Mod
+ * v1.2.0-dev7.jar!\com\parzi\starwarsmod\rendering\RenderDroidAstromech.class
+ * Java compiler version: 6 (50.0) JD-Core Version: 0.7.1
+ */
