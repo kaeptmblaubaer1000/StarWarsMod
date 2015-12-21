ee18d86787fb465ac98c657bf8299f0e7e9a72d0
Colby Newman <colby.newman2000@gmail.com>
2015-12-21 13:00:34 -0500

    bob

--- src/main/java/com/parzi/starwarsmod/vehicles/VehicleLandBase.java
+++ src/main/java/com/parzi/starwarsmod/vehicles/VehicleLandBase.java
@@ -1,6 +1,7 @@
 package com.parzi.starwarsmod.vehicles;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
@@ -22,22 +23,83 @@ public class VehicleLandBase extends VehicleBase
 			this.rotationYawHead = this.renderYawOffset = this.rotationYaw;
 			p_70612_1_ = ((EntityLivingBase)this.riddenByEntity).moveStrafing * 0.5F;
 			p_70612_2_ = ((EntityLivingBase)this.riddenByEntity).moveForward * (this.moveModifier / 8.0F);
-			if (this.onGround)
-			{
-				float f2 = MathHelper.sin(this.rotationYaw * 3.1415927F / 180.0F);
-				float f3 = MathHelper.cos(this.rotationYaw * 3.1415927F / 180.0F);
-				this.motionX += -0.4F * f2 * p_70612_2_;
-				this.motionZ += 0.4F * f3 * p_70612_2_;
-			}
+
+			float f2a = MathHelper.sin(this.rotationYaw * 3.1415927F / 180.0F);
+			float f3a = MathHelper.cos(this.rotationYaw * 3.1415927F / 180.0F);
+			this.motionX += -0.4F * f2a * p_70612_2_;
+			this.motionZ += 0.4F * f3a * p_70612_2_;
+
 			this.stepHeight = 1.0F;
-			this.jumpMovementFactor = this.getAIMoveSpeed() * 0.1F;
 			if (!this.worldObj.isRemote)
 			{
-				this.setAIMoveSpeed(p_70612_2_);
-				super.moveEntityWithHeading(p_70612_1_, p_70612_2_);
+				double d0;
+				float f2 = 0.91F;
+
+				if (this.onGround)
+				{
+					f2 = this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.boundingBox.minY) - 1, MathHelper.floor_double(this.posZ)).slipperiness * 0.91F;
+				}
+
+				float f3 = 0.16277136F / (f2 * f2 * f2);
+				float f4;
+
+				if (this.onGround)
+				{
+					f4 = this.getAIMoveSpeed() * f3;
+				}
+				else
+				{
+					f4 = this.jumpMovementFactor;
+				}
+
+				this.moveFlying(p_70612_1_, p_70612_2_, f4);
+				f2 = 0.91F;
+
+				if (this.onGround)
+				{
+					f2 = this.worldObj.getBlock(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.boundingBox.minY) - 1, MathHelper.floor_double(this.posZ)).slipperiness * 0.91F;
+				}
+
+				this.moveEntity(this.motionX, this.motionY, this.motionZ);
+
+				if (this.isCollidedHorizontally && this.isOnLadder())
+				{
+					this.motionY = 0.2D;
+				}
+
+				if (this.worldObj.isRemote && (!this.worldObj.blockExists((int)this.posX, 0, (int)this.posZ) || !this.worldObj.getChunkFromBlockCoords((int)this.posX, (int)this.posZ).isChunkLoaded))
+				{
+					if (this.posY > 0.0D)
+					{
+						this.motionY = -0.1D;
+					}
+					else
+					{
+						this.motionY = 0.0D;
+					}
+				}
+				else
+				{
+					this.motionY -= 1D;
+				}
+
+				this.motionY *= 0.9800000190734863D;
+				this.motionX *= (double)f2;
+				this.motionZ *= (double)f2;
+
+				this.prevLimbSwingAmount = this.limbSwingAmount;
+				d0 = this.posX - this.prevPosX;
+				double d1 = this.posZ - this.prevPosZ;
+				float f6 = MathHelper.sqrt_double(d0 * d0 + d1 * d1) * 4.0F;
+
+				if (f6 > 1.0F)
+				{
+					f6 = 1.0F;
+				}
+
+				this.limbSwingAmount += (f6 - this.limbSwingAmount) * 0.4F;
+				this.limbSwing += this.limbSwingAmount;
 			}
 		}
-		else
-			super.moveEntityWithHeading(p_70612_1_, p_70612_2_);
 	}
 }
