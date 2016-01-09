e59cf2fea662b05ebff4816ca25954264f1a98d1
Colby Newman <colby.newman2000@gmail.com>
2016-01-09 08:49:46 -0500

    stuff

--- src/main/java/com/parzi/starwarsmod/blocks/BlockDeathStar.java
+++ src/main/java/com/parzi/starwarsmod/blocks/BlockDeathStar.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.parzi.starwarsmod.Resources;
 import com.parzi.starwarsmod.StarWarsMod;
+import com.parzi.util.block.BlockMultiTexture;
 import com.parzi.util.world.HarvestLevel;
 
 import cpw.mods.fml.relauncher.Side;
@@ -16,56 +17,14 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 
-public class BlockDeathStar extends Block
+public class BlockDeathStar extends BlockMultiTexture
 {
-	public String name = "deathStarBlock";
-	private String[] names = { "HangarFloor", "LightHangarFloor", "HangarCrate", "CorridorFloor", "CautionFloor", "HangarWallPanel", "CorridorWallPanel", "ShieldGeneratorConsole", "Extra1", "Extra2", "Extra3" };
-	private int subtypes = this.names.length;
-	@SideOnly(Side.CLIENT)
-	private IIcon[] icons;
-
 	public BlockDeathStar()
 	{
-		super(Material.rock);
-		this.setBlockName(Resources.MODID + "." + this.name);
+		super("deathStarBlock", new String[] { "HangarFloor", "LightHangarFloor", "HangarCrate", "CorridorFloor", "CautionFloor", "HangarWallPanel", "CorridorWallPanel", "ShieldGeneratorConsole", "Extra1", "Extra2", "Extra3" }, Material.rock);
 		this.setCreativeTab(StarWarsMod.StarWarsTab);
 		this.setHardness(4.0F);
 		this.setHarvestLevel("pickaxe", HarvestLevel.IRON);
 		this.setStepSound(soundTypeMetal);
 	}
-
-	@Override
-	public int damageDropped(int meta)
-	{
-		return meta;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int side, int meta)
-	{
-		return this.icons[meta];
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void getSubBlocks(Item item, CreativeTabs tab, List metaTypes)
-	{
-		for (int i = 0; i < this.subtypes; i++)
-			metaTypes.add(new ItemStack(item, 1, i));
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister par1IconRegister)
-	{
-		this.icons = new IIcon[this.subtypes];
-		for (int i = 0; i < this.icons.length; i++)
-			this.icons[i] = par1IconRegister.registerIcon(Resources.MODID + ":" + this.name + this.names[i]);
-	}
 }
-/*
- * Location: C:\Users\Colby\Downloads\Parzi's Star Wars Mod
- * v1.2.0-dev7.jar!\com\parzi\starwarsmod\blocks\BlockEndorBaseWall.class Java
- * compiler version: 6 (50.0) JD-Core Version: 0.7.1
- */
--- src/main/java/com/parzi/starwarsmod/blocks/BlockDeathStarLight.java
+++ src/main/java/com/parzi/starwarsmod/blocks/BlockDeathStarLight.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.parzi.starwarsmod.Resources;
 import com.parzi.starwarsmod.StarWarsMod;
+import com.parzi.util.block.BlockLitMultiTexture;
 import com.parzi.util.world.HarvestLevel;
 
 import cpw.mods.fml.relauncher.Side;
@@ -16,57 +17,14 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 
-public class BlockDeathStarLight extends Block
+public class BlockDeathStarLight extends BlockLitMultiTexture
 {
-	public String name = "deathStarLight";
-	private String[] names = { "HangarFloorSmall", "PureWhite", "HangarWallVertical", "Extra1", "Extra2" };
-	private int subtypes = this.names.length;
-	@SideOnly(Side.CLIENT)
-	private IIcon[] icons;
-
 	public BlockDeathStarLight()
 	{
-		super(Material.rock);
-		this.setBlockName(Resources.MODID + "." + this.name);
+		super("deathStarLight", new String[] { "HangarFloorSmall", "PureWhite", "HangarWallVertical", "Extra1", "Extra2" }, 1, Material.rock);
 		this.setCreativeTab(StarWarsMod.StarWarsTab);
 		this.setHardness(4.0F);
 		this.setHarvestLevel("pickaxe", HarvestLevel.IRON);
 		this.setStepSound(soundTypeMetal);
-		this.setLightLevel(1.0F);
-	}
-
-	@Override
-	public int damageDropped(int meta)
-	{
-		return meta;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int side, int meta)
-	{
-		return this.icons[meta];
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void getSubBlocks(Item item, CreativeTabs tab, List metaTypes)
-	{
-		for (int i = 0; i < this.subtypes; i++)
-			metaTypes.add(new ItemStack(item, 1, i));
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister par1IconRegister)
-	{
-		this.icons = new IIcon[this.subtypes];
-		for (int i = 0; i < this.icons.length; i++)
-			this.icons[i] = par1IconRegister.registerIcon(Resources.MODID + ":" + this.name + this.names[i]);
 	}
 }
-/*
- * Location: C:\Users\Colby\Downloads\Parzi's Star Wars Mod
- * v1.2.0-dev7.jar!\com\parzi\starwarsmod\blocks\BlockEndorBaseWall.class Java
- * compiler version: 6 (50.0) JD-Core Version: 0.7.1
- */
