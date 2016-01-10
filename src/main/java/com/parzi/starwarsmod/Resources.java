591cd0c6836f055c8365e4e6925c7aaae5782208
Colby Newman <colby.newman2000@gmail.com>
2016-01-10 15:26:15 -0500

    dev7

--- src/main/java/com/parzi/starwarsmod/Resources.java
+++ src/main/java/com/parzi/starwarsmod/Resources.java
@@ -7,7 +7,7 @@ public class Resources
 	// Core
 	public static final String MODID = "starwarsmod";
 	public static final String VERSION = "1.2.5";
-	public static final String DEV_VER = "dev6";
+	public static final String DEV_VER = "dev7";
 	public static String ONLINE_VERSION = "";
 
 	public static boolean IS_DEV_ENVIRONVENT = false;
