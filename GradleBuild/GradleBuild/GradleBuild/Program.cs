4922165f55f1d60bfa807b9f566e249e6a5e0f2f
Colby Newman <colby.newman2000@gmail.com>
2016-01-30 15:55:02 -0500

    builder update

--- GradleBuild/GradleBuild/GradleBuild/Program.cs
+++ GradleBuild/GradleBuild/GradleBuild/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace GradleBuild
@@ -13,30 +14,61 @@ namespace GradleBuild
         private static void Main(string[] args)
         {
             Stopwatch stopwatch = new Stopwatch();
-            Console.Write("v > ");
-            string str = Console.ReadLine();
-            stopwatch.Start();
-            Console.WriteLine("Building Gradle...");
-            Process process = new Process();
-            process.StartInfo = new ProcessStartInfo("gradlew.bat", "build");
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.Start();
-            process.WaitForExit();
-            Console.WriteLine(process.StandardOutput.ReadToEnd());
-            string fname = "releases/Parzi's Star Wars Mod v" + str + "-dev{0}.jar";
-            int dev = 1;
-            while (File.Exists(string.Format(fname, dev)))
-                dev++;
-            fname = string.Format(fname, dev);
-            Console.WriteLine("Creating jar: {0}", fname);
-            if (File.Exists("build/libs/starwarsmod-1.0.jar"))
-                File.Move("build/libs/starwarsmod-1.0.jar", fname);
-            stopwatch.Stop();
-            Console.WriteLine("Finished packing in " + ((float)stopwatch.ElapsedMilliseconds / 1000f).ToString() + "s");
-            Console.ReadKey();
+            Console.Write("Parsing build.gradle... ");
+            if (File.Exists("build.gradle"))
+            {
+                using (StreamReader r = new StreamReader("build.gradle"))
+                {
+                    Match m = new Regex("version.+\"((?:\\d\\.?)+)\"").Match(r.ReadToEnd());
+                    if (m.Success)
+                    {
+                        string ver = m.Groups[1].Value;
+
+                        string fname = "releases/Parzi's Star Wars Mod v" + ver + "-dev{0}.jar";
+                        int dev = 1;
+                        while (File.Exists(string.Format(fname, dev)))
+                            dev++;
+
+                        Console.WriteLine("Done!");
+
+                        stopwatch.Start();
+                        Console.WriteLine("Compiling version {0}-dev{1}...", ver, dev);
+                        if (File.Exists("gradlew.bat"))
+                        {
+                            Process process = new Process();
+                            process.StartInfo = new ProcessStartInfo("gradlew.bat", "build");
+                            process.StartInfo.UseShellExecute = false;
+                            process.StartInfo.RedirectStandardError = true;
+                            process.StartInfo.RedirectStandardInput = true;
+                            process.StartInfo.RedirectStandardOutput = true;
+                            process.Start();
+                            process.WaitForExit();
+                            Console.WriteLine(process.StandardOutput.ReadToEnd());
+                            fname = string.Format(fname, dev);
+                            Console.WriteLine("Moving jar: {0}", fname);
+                            if (File.Exists("build/libs/starwarsmod-" + ver + ".jar"))
+                                File.Move("build/libs/starwarsmod-" + ver + ".jar", fname);
+                            else
+                                Console.WriteLine("Unable to locate compiled jar!");
+                            stopwatch.Stop();
+                            Console.WriteLine("Finished in " + ((float)stopwatch.ElapsedMilliseconds / 1000f).ToString() + "s");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Unable to locate gradlew.bat!");
+                        }
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to parse build.gradle!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unable to locate build.gradle!");
+            }
         }
     }
 }
--- src/main/java/com/parzivail/pswm/rendering/vehicles/RenderXWing.java
+++ src/main/java/com/parzivail/pswm/rendering/vehicles/RenderXWing.java
@@ -1,11 +1,19 @@
 package com.parzivail.pswm.rendering.vehicles;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.entity.RenderSlime;
+import net.minecraft.client.shader.ShaderGroup;
+import net.minecraft.client.util.JsonException;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
 
+import org.lwjgl.opengl.GL11;
+
 import com.parzivail.pswm.Resources;
+import com.parzivail.pswm.StarWarsMod;
 import com.parzivail.pswm.models.vehicles.ModelXWing;
 import com.parzivail.pswm.vehicles.VehicXWing;
 
@@ -22,6 +30,12 @@ public class RenderXWing extends RenderVehicAirBase
 	protected ResourceLocation getEntityTexture(Entity entity)
 	{
 		return texture;
+	}
+
+	@Override
+	public void doRender(Entity p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_)
+	{
+		super.doRender(p_76986_1_, p_76986_2_, p_76986_4_, p_76986_6_, p_76986_8_, p_76986_9_);
 	}
 
 	@Override
