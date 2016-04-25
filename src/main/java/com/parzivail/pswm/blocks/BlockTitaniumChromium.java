13dced75ed0ed95a0c78e079f67da99cc68a268c
Colby Newman <colby.newman2000@gmail.com>
2016-04-25 07:24:01 -0400

    whoops

--- src/main/java/com/parzivail/pswm/blocks/BlockTitaniumChromium.java
+++ src/main/java/com/parzivail/pswm/blocks/BlockTitaniumChromium.java
@@ -22,8 +22,3 @@ public class BlockTitaniumChromium extends Block
 		setStepSound(soundTypeStone);
 	}
 }
-/*
- * Location: C:\Users\Colby\Downloads\Parzi's Star Wars Mod
- * v1.2.0-dev7.jar!\com\parzi\starwarsmod\blocks\BlockTitaniumChromium.class
- * Java compiler version: 6 (50.0) JD-Core Version: 0.7.1
- */
