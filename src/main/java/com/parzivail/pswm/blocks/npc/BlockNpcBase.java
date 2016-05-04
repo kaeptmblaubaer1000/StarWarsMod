66d021a2718b2f2516e02c1bd676ec1f0eaf90f7
Colby Newman <colby.newman2000@gmail.com>
2016-05-04 10:13:06 -0400

    rotation lock work + debug info

--- src/main/java/com/parzivail/pswm/blocks/npc/BlockNpcBase.java
+++ src/main/java/com/parzivail/pswm/blocks/npc/BlockNpcBase.java
@@ -4,13 +4,17 @@ import com.parzivail.pswm.Resources;
 import com.parzivail.pswm.StarWarsMod;
 import com.parzivail.pswm.tileentities.TileEntityStaticNpc;
 import com.parzivail.util.IDebugProvider;
+import com.parzivail.util.ui.Lumberjack;
 import com.parzivail.util.world.HarvestLevel;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -41,7 +45,9 @@ public class BlockNpcBase extends BlockContainer implements IDebugProvider
 		if (tile instanceof TileEntityStaticNpc)
 		{
 			TileEntityStaticNpc t = (TileEntityStaticNpc)tile;
-			list.add("Unnamed NPC");
+			list.add(t.getId());
+			list.add(String.format("Facing: %s", t.getFacing()));
+			list.add(String.format("Locked: %s", t.getLocked()));
 		}
 
 		return list;
@@ -56,9 +62,34 @@ public class BlockNpcBase extends BlockContainer implements IDebugProvider
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float subX, float subY, float subZ)
 	{
-		if (!world.isRemote)
-			player.openGui(StarWarsMod.instance, Resources.GUI_QUESTLOG, world, x, y, z);
+		if (player.isSneaking())
+		{
+			TileEntity tile = world.getTileEntity(x, y, z);
+			if (tile instanceof TileEntityStaticNpc)
+			{
+				TileEntityStaticNpc t = (TileEntityStaticNpc)tile;
+				t.setLocked(!t.getLocked());
+			}
+		}
+		else
+		{
+			if (!world.isRemote)
+				player.openGui(StarWarsMod.instance, Resources.GUI_QUESTLOG, world, x, y, z);
+		}
 		return true;
+	}
+
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack item)
+	{
+		TileEntity tile = world.getTileEntity(x, y, z);
+		if (tile instanceof TileEntityStaticNpc)
+		{
+			TileEntityStaticNpc tile1 = (TileEntityStaticNpc)tile;
+			int l = MathHelper.floor_double(player.rotationYaw % 360 / 90f + 0.5f);
+			tile1.setFacing(l);
+			Lumberjack.log("placed at " + String.valueOf(tile1.getFacing()));
+		}
 	}
 
 	@Override
--- src/main/java/com/parzivail/pswm/rendering/RenderStaticNpc.java
+++ src/main/java/com/parzivail/pswm/rendering/RenderStaticNpc.java
@@ -33,7 +33,10 @@ public class RenderStaticNpc extends TileEntitySpecialRenderer
 			TileEntityStaticNpc staticNpc = (TileEntityStaticNpc)te;
 			GL11.glPushMatrix();
 			GL11.glTranslated(x + 0.5f, y, z + 0.5f);
-			GL11.glRotatef(staticNpc.getAngleToClosestPlayer(), 0, 1, 0);
+			if (staticNpc.getLocked())
+				GL11.glRotatef(staticNpc.getFacing() * 90, 0, 1, 0);
+			else
+				GL11.glRotatef(staticNpc.getAngleToClosestPlayer(), 0, 1, 0);
 			biped.doRender(staticNpc.getInternalEntity(), 0, 0, 0, 0, 0.0625f);
 			GL11.glPopMatrix();
 		}
