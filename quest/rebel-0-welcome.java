d4dc6deb8763bf384efe2b2079243d12adac3970
Colby Newman <colby.newman2000@gmail.com>
2016-05-08 18:53:39 -0400

    quests finally start

---
+++ quest/rebel-0-welcome.java
@@ -0,0 +1,2 @@
+this.tree = new DialogTree();
+this.tree.npcHeader = "Welcome to the Rebel Alliance. We're a small group dedicated to taking back the galaxy from the Galactic Empire. If you want to join us, just find the Quartermaster and he'll give you some armor. You can find him in the main hangar, good luck kid.";
