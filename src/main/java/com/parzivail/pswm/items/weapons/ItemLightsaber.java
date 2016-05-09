66e6eefb3a72e01a8bcf682c6212c02484fac2d7
weastonmiller <westonmiller@yahoo.com>
2016-05-09 14:51:04 -0400

    green adjustment

--- src/main/java/com/parzivail/pswm/items/weapons/ItemLightsaber.java
+++ src/main/java/com/parzivail/pswm/items/weapons/ItemLightsaber.java
@@ -38,7 +38,7 @@ public class ItemLightsaber extends ItemSword
 	public static int[] colorHex = { 0xFFFF00, 0xFF4F89, 0xE066FF, 0xF2F2F2, 0x595959, 0xFF5A00, 0x00E5EE, 0x191919, 0xFF0000, 0x00FF00, 0x0000FF };
 	// public static final String[] colorName = { "yellow", "pink", "purple",
 	// "white", "gray", "orange", "teal", "black", "red", "green", "blue" };
-	public static int[] colorHexNew = { 0xFFFF2B, 0xFF00DC, 0xB069DB, 0xFFFFFF, 0xA0A0A0, 0x202020, 0xFF6A00, 0x2FD0F6, 0xFF0000, 0x00FF48, 0x2448DA, 0xFF5200 };
+	public static int[] colorHexNew = { 0xFFFF2B, 0xFF00DC, 0xB069DB, 0xFFFFFF, 0xA0A0A0, 0x202020, 0xFF6A00, 0x2FD0F6, 0xFF0000, 0x00FF30, 0x2448DA, 0xFF5200 };
 
 	static
 	{
@@ -184,7 +184,7 @@ public class ItemLightsaber extends ItemSword
 		if (s.equals("maul") || s.equals("dooku") || s.equals("vader2") || s.equals("malgus") || s.equals("doubleSith") || s.equals("starkiller") || s.equals("shoto"))
 			nbt.setInteger(nbtBladeColor, 0xFF0000); // red
 		else if (s.equals("quigon") || s.equals("padawan") || s.equals("luke2"))
-			nbt.setInteger(nbtBladeColor, 0x00FF48); // green
+			nbt.setInteger(nbtBladeColor, 0x00FF30); // green
 		else if (s.equals("obiwan") || s.equals("luke1") || s.equals("kanan") || s.equals("ezra") || s.equals("revan"))
 			nbt.setInteger(nbtBladeColor, 0x2448DA); // blue
 		else if (s.equals("crossguard"))
