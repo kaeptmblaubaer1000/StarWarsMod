0ad2d406d59f9fb7a381ca47bc1e4091f998bca7
Colby Newman <colby.newman2000@gmail.com>
2016-05-13 15:10:18 -0400

    line fixing

--- src/main/java/com/parzivail/pswm/rendering/gui/AnimationTargetingComputer.java
+++ src/main/java/com/parzivail/pswm/rendering/gui/AnimationTargetingComputer.java
@@ -24,6 +24,8 @@ public class AnimationTargetingComputer extends Animation
 		super.render(event);
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 
+		GL11.glLineWidth(3);
+
 		GL11.glColor3f(0, 0, 0);
 		int x = 0;
 		int y = 50;
@@ -32,13 +34,14 @@ public class AnimationTargetingComputer extends Animation
 		Screen2D.drawRectangle(x, y, width, height, true);
 
 		GL11.glColor3f(1, 1, 0);
-		Screen2D.drawLine(x + 10, y + 10, x + width - 10, y + height - 10);
-		Screen2D.drawLine(x + 10, y + height - 10, x + width - 10, y + 10);
+		Screen2D.drawLine(x + 60, y + 10, x + width - 60, y + height - 10);
+		Screen2D.drawLine(x + 60, y + height - 10, x + width - 60, y + 10);
 
-		Screen2D.drawLine(x + 10, y + 30, x + width - 10, y + height - 30);
-		Screen2D.drawLine(x + 10, y + height - 30, x + width - 10, y + 30);
+		Screen2D.drawLine(x + 20, y + 10, x + width - 20, y + height - 10);
+		Screen2D.drawLine(x + 20, y + height - 10, x + width - 20, y + 10);
 
-		Screen2D.drawLine(x + 10, y + height / 2f, x + width - 10, y + height / 2f);
+		Screen2D.drawLine(x + 10, y + 40, x + width - 10, y + height - 40);
+		Screen2D.drawLine(x + 10, y + height - 40, x + width - 10, y + 40);
 
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 	}
