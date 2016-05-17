27ae566710ce9d3ef1c029bfdf2f7b543c46e7e3
Colby Newman <colby.newman2000@gmail.com>
2016-05-17 19:46:07 -0400

    STOP IT

--- src/main/java/com/parzivail/pswm/models/lightsabers/ModelInquisitorHilt.java
+++ src/main/java/com/parzivail/pswm/models/lightsabers/ModelInquisitorHilt.java
@@ -1,14 +1,26 @@
-package starwarsmod;
+package com.parzivail.pswm.models.lightsabers;
 
+import com.parzivail.pswm.Resources;
+import com.parzivail.pswm.rendering.IHandlesRender;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.IItemRenderer;
+import org.lwjgl.opengl.GL11;
 
 /**
  * NewProject - Undefined
  * Created using Tabula 4.1.1
  */
-public class ModelInquisitorHilt extends ModelBase {
+public class ModelInquisitorHilt extends ModelBase implements IHandlesRender
+{
+
+	ResourceLocation tA = new ResourceLocation(Resources.MODID, "textures/models/lightsabers/sith_A.png");
+	ResourceLocation tB = new ResourceLocation(Resources.MODID, "textures/models/lightsabers/sith_B.png");
+
     public ModelRenderer Shape1;
     public ModelRenderer Shape2;
     public ModelRenderer Shape3;
@@ -378,12 +390,104 @@ public class ModelInquisitorHilt extends ModelBase {
         this.Shape10.render(f5);
     }
 
-    /**
-     * This is a helper function from Tabula to set the rotation of model parts
-     */
-    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-        modelRenderer.rotateAngleX = x;
-        modelRenderer.rotateAngleY = y;
-        modelRenderer.rotateAngleZ = z;
-    }
+	@Override
+	public ResourceLocation getResourceLocation(boolean alt)
+	{
+		if (alt)
+			return this.tB;
+		return this.tA;
+	}
+
+	@Override
+	public boolean handleRenderType(ItemStack item, IItemRenderer.ItemRenderType type)
+	{
+		return true;
+	}
+
+	@Override
+	public void renderItem(IItemRenderer.ItemRenderType type, ItemStack item, Object... data)
+	{
+		switch (type)
+		{
+			case ENTITY:
+				GL11.glPushMatrix();
+				GL11.glScalef(0.03f, 0.03f, 0.03f);
+				GL11.glRotatef(-90, 0, 0, 1);
+				GL11.glTranslatef(-40, -2, -2);
+				this.render(null, 0, 0, 0, 0, 0, 0.625f);
+				GL11.glPopMatrix();
+				break;
+			case EQUIPPED:
+				GL11.glPushMatrix();
+				GL11.glScalef(0.05f, 0.05f, 0.05f);
+				GL11.glRotatef(180, 1, 0, 0);
+				GL11.glRotatef(45, 0, 1, 0);
+				GL11.glRotatef(20, 0, 0, 1);
+				GL11.glRotatef(90, 1, 0, 0);
+				GL11.glTranslatef(22, -1.5f, 20);
+				if (data[1] instanceof EntityPlayer)
+				{
+					EntityPlayer player = (EntityPlayer)data[1];
+					if (player.isBlocking())
+					{
+						GL11.glRotatef(80, 0, 0, 1);
+						GL11.glTranslatef(0, -2, 0);
+					}
+				}
+				GL11.glRotatef(-90, 1, 0, 0);
+				this.render(null, 0, 0, 0, 0, 0, 0.625f);
+				GL11.glPopMatrix();
+				break;
+			case EQUIPPED_FIRST_PERSON:
+				GL11.glPushMatrix();
+				GL11.glScalef(0.05f, 0.05f, 0.05f);
+				GL11.glTranslatef(0, 25, 0);
+				GL11.glRotatef(-130, 0, 1, 0);
+				GL11.glRotatef(180, 1, 0, 0);
+				GL11.glRotatef(75, 0, 0, 1);
+				GL11.glRotatef(85, 1, 0, 0);
+				GL11.glTranslatef(25, 18, 0);
+				if (data[1] instanceof EntityPlayer)
+				{
+					EntityPlayer player = (EntityPlayer)data[1];
+					if (player.isBlocking())
+					{
+						GL11.glRotatef(-20, 0, 0, 1);
+						GL11.glRotatef(70, 0, 1, 0);
+						GL11.glRotatef(30, 1, 0, 0);
+						GL11.glTranslatef(10, -4, -5);
+					}
+				}
+				GL11.glRotatef(-90, 1, 0, 0);
+				this.render(null, 0, 0, 0, 0, 0, 0.625f);
+				GL11.glPopMatrix();
+				break;
+			case INVENTORY:
+				GL11.glScalef(0.035f, 0.035f, 0.035f);
+				GL11.glTranslatef(-2, -3, -1);
+				GL11.glRotatef(160, 0, 0, 1);
+				GL11.glRotatef(180, 0, 1, 0);
+				GL11.glRotatef(90, 1, 0, 0);
+				this.render(null, 0, 0, 0, 0, 0, 0.625f);
+				break;
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * This is a helper function from Tabula to set the rotation of model parts
+	 */
+	public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z)
+	{
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+
+	@Override
+	public boolean shouldUseRenderHelper(IItemRenderer.ItemRenderType type, ItemStack item, IItemRenderer.ItemRendererHelper helper)
+	{
+		return true;
+	}
 }
