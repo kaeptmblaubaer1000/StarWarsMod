526c9b6615a21b1b6c06dbbd5fcf461a22ce8360
Colby Newman <colby.newman2000@gmail.com>
2016-05-21 13:41:26 -0400

    new jedi robes and lightsaber tooltip

--- src/main/java/com/parzivail/pswm/jedi/ArmorJediRobes.java
+++ src/main/java/com/parzivail/pswm/jedi/ArmorJediRobes.java
@@ -7,18 +7,25 @@ import com.parzivail.pswm.rendering.force.ModelJediCloak;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+import java.util.List;
 
 public class ArmorJediRobes extends ItemArmor
 {
-
+	@SideOnly(Side.CLIENT)
+	private IIcon[] icons;
 	@SideOnly(Side.CLIENT)
 	public static ModelCompressionArmor model;
 
-	String name = "newJediRobes";
+	private String name = "newJediRobes";
 
 	public ArmorJediRobes()
 	{
@@ -26,6 +33,7 @@ public class ArmorJediRobes extends ItemArmor
 		this.setUnlocalizedName(Resources.MODID + "." + this.name);
 		this.setTextureName(Resources.MODID + ":" + this.name);
 		this.setCreativeTab(StarWarsMod.StarWarsTab);
+		this.setHasSubtypes(true);
 	}
 
 	@Override
@@ -43,6 +51,31 @@ public class ArmorJediRobes extends ItemArmor
 	@Override
 	public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type)
 	{
+		if (stack != null)
+			return Resources.MODID + ":" + "textures/force/cloak" + stack.getItemDamage() + ".png";
 		return Resources.MODID + ":" + "textures/force/cloak.png";
+	}
+
+	@Override
+	public IIcon getIconFromDamage(int par1)
+	{
+		return this.icons[par1];
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, List par3List)
+	{
+		for (int i = 0; i < 6; i++)
+			par3List.add(new ItemStack(this, 1, i));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister par1IconRegister)
+	{
+		this.icons = new IIcon[6];
+		for (int i = 0; i < this.icons.length; i++)
+			this.icons[i] = par1IconRegister.registerIcon(Resources.MODID + ":" + this.name + "_" + i);
 	}
 }
