667d5adaaf6ccf268ecbc83717d4b46604dad0df
weastonmiller <westonmiller@yahoo.com>
2016-05-22 11:59:41 -0400

    bomber positioning

--- src/main/java/com/parzivail/pswm/rendering/item/RenderSpawnTIEBomber.java
+++ src/main/java/com/parzivail/pswm/rendering/item/RenderSpawnTIEBomber.java
@@ -37,8 +37,8 @@ public class RenderSpawnTIEBomber implements IItemRenderer
 			case INVENTORY:
 				GL11.glPushMatrix();
 				GL11.glDisable(GL11.GL_CULL_FACE);
-				GL11.glScalef(0.04F, -0.04F, 0.04F);
-				GL11.glTranslatef(-6, 3.0f, 1f);
+				GL11.glScalef(0.03F, -0.03F, 0.03F);
+				GL11.glTranslatef(-2, -1, 1f);
 				GL11.glRotatef(0, 0, 0, 1);
 				GL11.glRotatef(260, 0, 1, 0);
 				GL11.glTranslatef(-10, 0, 0);
@@ -52,7 +52,7 @@ public class RenderSpawnTIEBomber implements IItemRenderer
 				{
 					GL11.glPushMatrix();
 					GL11.glDisable(GL11.GL_CULL_FACE);
-					GL11.glScalef(0.03F, -0.03F, 0.03F);
+					GL11.glScalef(0.0215F, -0.0215F, 0.0215F);
 					GL11.glRotatef(-40, 0, 1, 0);
 					GL11.glRotatef(24, 0, 0, 1);
 					GL11.glTranslatef(19, 7, 0);
@@ -60,7 +60,7 @@ public class RenderSpawnTIEBomber implements IItemRenderer
 					GL11.glRotatef(90, 0, 0, -1);
 					GL11.glRotatef(90, 1, 0, 0);
 					GL11.glRotatef(88, -1, 0, 0);
-					GL11.glTranslatef(-2.75f, 3, 34);
+					GL11.glTranslatef(-10, 12, 42);
 					GL11.glScalef(-1, -1, 1);
 					GL11.glRotatef(90, 0, 0, 1);
 					GL11.glRotatef(90, 0, 0, 1);
@@ -89,15 +89,15 @@ public class RenderSpawnTIEBomber implements IItemRenderer
 			case EQUIPPED_FIRST_PERSON:
 				GL11.glPushMatrix();
 				GL11.glDisable(GL11.GL_CULL_FACE);
-				GL11.glScalef(0.02F, -0.02F, 0.02F);
-				GL11.glTranslatef(8, -53, 29);
+				GL11.glScalef(0.0165F, -0.0165F, 0.0165F);
+				GL11.glTranslatef(10, -58, 31);
 				GL11.glRotatef(180, 0, 0, 1);
 				GL11.glRotatef(60, 0, 1, 0);
 				GL11.glRotatef(25, 1, 0, 0);
 				GL11.glRotatef(-4, 0, 1, 0);
 				GL11.glTranslatef(8, 10, 6);
 				GL11.glScalef(-1, -1, -1);
-				GL11.glRotatef(-30, 1, 0.1f, 0);
+				GL11.glRotatef(-30, 1, 0.2f, 0);
 				this.model.render(null, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.625F);
 				GL11.glEnable(GL11.GL_CULL_FACE);
 				GL11.glPopMatrix();
