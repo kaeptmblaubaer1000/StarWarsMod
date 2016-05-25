364f52f229c78f6c11225f53dc1577974fd9e2a8
Colby Newman <colby.newman2000@gmail.com>
2016-05-25 06:53:17 -0400

    resources

---
+++ resources/show_gource.bat
@@ -0,0 +1,5 @@
+gource -a 1 -s 1 --camera-mode overview --hide bloom
+REM -a: autoskip seconds = 1
+REM -s: seconds per day  = 1
+REM --camera-move        = overview (all fit onscreen)
+REM --hide               = bloom (disable bloom)
