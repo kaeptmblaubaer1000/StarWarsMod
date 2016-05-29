b774569d877effa0112ed4c86061d8dca85accfc
Colby Newman <colby.newman2000@gmail.com>
2016-05-29 19:26:59 -0400

    paths n splines

---
+++ resources/documents/paths-moseisley.java
@@ -0,0 +1,13 @@
+new FPoint(3, 69, 1), new FPoint(11, 69, 11), new FPoint(18, 69, 19), new FPoint(26, 69, 32), new FPoint(36, 69, 43), new FPoint(45, 69, 57), new FPoint(58, 69, 66), new FPoint(69, 69, 85), new FPoint(84, 69, 100), new FPoint(91, 69, 107), new FPoint(92, 69, 124), new FPoint(93, 69, 146), new FPoint(98, 69, 154), new FPoint(108, 69, 172), new FPoint(121, 69, 190), new FPoint(122, 69, 215), new FPoint(126, 69, 232), new FPoint(127, 69, 250), new FPoint(134, 69, 264), new FPoint(141, 69, 281), new FPoint(140, 70, 349)
+
+new FPoint(3, 69, 1), new FPoint(22, 69, 20), new FPoint(25, 69, 36), new FPoint(39, 69, 48), new FPoint(44, 69, 59), new FPoint(21, 69, 79), new FPoint(4, 69, 94), new FPoint(-13, 71, 114), new FPoint(-62, 69, 133)
+
+new FPoint(4, 69, 3), new FPoint(45, 69, 57), new FPoint(66, 69, 74), new FPoint(92, 69, 105), new FPoint(92, 69, 142), new FPoint(98, 69, 155), new FPoint(68, 69, 166), new FPoint(0, 69, 169)
+
+new FPoint(3, 69, 1), new FPoint(31, 69, 38), new FPoint(49, 69, 61), new FPoint(71, 69, 84), new FPoint(92, 69, 106), new FPoint(94, 69, 146), new FPoint(97, 69, 156), new FPoint(58, 69, 170), new FPoint(46, 69, 176), new FPoint(32, 69, 191), new FPoint(40, 69, 216), new FPoint(42, 69, 225), new FPoint(29, 69, 239), new FPoint(30, 69, 260), new FPoint(29, 69, 264), new FPoint(29, 69, 273)
+
+new FPoint(3, 69, 1), new FPoint(27, 69, 31), new FPoint(38, 69, 49), new FPoint(65, 69, 74), new FPoint(82, 69, 98), new FPoint(92, 69, 115), new FPoint(94, 69, 146), new FPoint(99, 69, 155), new FPoint(83, 69, 161), new FPoint(45, 69, 177), new FPoint(37, 69, 208), new FPoint(55, 69, 242), new FPoint(73, 69, 240), new FPoint(92, 69, 242), new FPoint(113, 69, 230), new FPoint(127, 69, 250), new FPoint(137, 69, 267)
+
+new FPoint(4, 69, 1), new FPoint(32, 69, 37), new FPoint(53, 69, 62), new FPoint(71, 69, 83), new FPoint(83, 69, 100), new FPoint(93, 69, 111), new FPoint(93, 69, 137), new FPoint(94, 69, 147), new FPoint(100, 69, 155), new FPoint(116, 69, 148), new FPoint(130, 69, 133), new FPoint(166, 69, 126), new FPoint(193, 69, 118)
+
+new FPoint(3, 69, 2), new FPoint(25, 69, 28), new FPoint(41, 69, 52), new FPoint(66, 69, 75), new FPoint(82, 69, 101), new FPoint(92, 69, 105), new FPoint(115, 69, 87), new FPoint(144, 69, 58), new FPoint(152, 69, 30), new FPoint(149, 69, 0)
