08382e4be36560e6921e23cb0c35599565e94358
Colby Newman <colby.newman2000@gmail.com>
2016-06-29 15:14:57 -0400

    aabb

--- src/main/java/com/parzivail/util/block/PBlockContainer.java
+++ src/main/java/com/parzivail/util/block/PBlockContainer.java
@@ -1,8 +1,12 @@
 package com.parzivail.util.block;
 
 import com.parzivail.pswm.Resources;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
 
 public abstract class PBlockContainer extends BlockContainer
 {
@@ -13,5 +17,25 @@ public abstract class PBlockContainer extends BlockContainer
 		super(material);
 		this.name = name;
 		this.setBlockName(Resources.MODID + "." + this.name);
+	}
+
+	/**
+	 * Returns the bounding box of the wired rectangular prism to render.
+	 */
+	@SideOnly(Side.CLIENT)
+	public AxisAlignedBB getSelectedBoundingBoxFromPool(World p_149633_1_, int p_149633_2_, int p_149633_3_, int p_149633_4_)
+	{
+		this.setBlockBoundsBasedOnState(p_149633_1_, p_149633_2_, p_149633_3_, p_149633_4_);
+		return super.getSelectedBoundingBoxFromPool(p_149633_1_, p_149633_2_, p_149633_3_, p_149633_4_);
+	}
+
+	/**
+	 * Returns a bounding box from the pool of bounding boxes (this means this box can change after the pool has been
+	 * cleared to be reused)
+	 */
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
+	{
+		this.setBlockBoundsBasedOnState(p_149668_1_, p_149668_2_, p_149668_3_, p_149668_4_);
+		return super.getCollisionBoundingBoxFromPool(p_149668_1_, p_149668_2_, p_149668_3_, p_149668_4_);
 	}
 }
