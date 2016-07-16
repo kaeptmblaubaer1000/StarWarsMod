362e9e1f3381642025cab0fcb2bc1c5afc924451
Colby Newman <colby.newman2000@gmail.com>
2016-07-16 15:00:36 -0400

    IF POWER GOES OUT AGAIN IM KILLING SOMEONE

--- src/main/java/com/parzivail/pswm/world/StructureBank.java
+++ src/main/java/com/parzivail/pswm/world/StructureBank.java
@@ -9,4 +9,5 @@ public class StructureBank
 {
 	public static Schematic yavinTemple = new Schematic("yavintemple", "mobs-map");
 	public static Schematic yodaTree = new Schematic("mobs-yodatree", "mobs-map");
+	public static Schematic falcon = new Schematic("falcon", "buildserver-map");
 }
