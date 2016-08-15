90582f48ec244577e51e92f2f0504be769e06f45
Colby Newman <colby.newman2000@gmail.com>
2016-08-15 12:35:32 -0400

    PSWMT-507 - remove langutils from commands

--- src/main/java/com/parzivail/pswm/commands/CommandJediRobes.java
+++ src/main/java/com/parzivail/pswm/commands/CommandJediRobes.java
@@ -2,7 +2,6 @@ package com.parzivail.pswm.commands;
 
 import com.parzivail.pswm.Resources;
 import com.parzivail.pswm.force.Cron;
-import com.parzivail.util.ui.LangUtils;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -46,7 +45,7 @@ public class CommandJediRobes extends CommandBase
 	{
 		if (astring.length != 2)
 		{
-			icommandsender.addChatMessage(new ChatComponentText(LangUtils.translate("invalid.args.usage.0", this.getCommandUsage(icommandsender))));
+			icommandsender.addChatMessage(new ChatComponentText(String.format("Invalid args! Usage: %s", this.getCommandUsage(icommandsender))));
 			return;
 		}
 
@@ -75,26 +74,26 @@ public class CommandJediRobes extends CommandBase
 				}
 			else
 				robes.stackTagCompound.setInteger(key, value);
-			icommandsender.addChatMessage(new ChatComponentText(LangUtils.translate("holocron.set.0.to.1", key, String.valueOf(value))));
+			icommandsender.addChatMessage(new ChatComponentText(String.format("[Holocron] Set %s to %s", key, String.valueOf(value))));
 
 			if (key.equalsIgnoreCase("level"))
 			{
 				robes.stackTagCompound.setInteger(Resources.nbtMaxXp, (value + 1) * 100);
-				icommandsender.addChatMessage(new ChatComponentText(LangUtils.translate("holocron.set.max.xp.to.0", String.valueOf((value + 1) * 100))));
+				icommandsender.addChatMessage(new ChatComponentText(String.format("[Holocron] Set Max XP to %s", String.valueOf((value + 1) * 100))));
 				robes.stackTagCompound.setInteger(Resources.nbtRemainingPts, value);
-				icommandsender.addChatMessage(new ChatComponentText(LangUtils.translate("holocron.set.remaining.upgrade.points.to.0", String.valueOf(value))));
+				icommandsender.addChatMessage(new ChatComponentText(String.format("[Holocron] Set remaining Upgrade Points to %s", String.valueOf(value))));
 			}
 		}
 		else
 		{
 			ItemStack robes = Cron.getHolocron(player);
-			icommandsender.addChatMessage(new ChatComponentText(LangUtils.translate("holocron.usage.0", this.getCommandUsage(icommandsender))));
+			icommandsender.addChatMessage(new ChatComponentText(String.format("[Holocron] Usage: %s", this.getCommandUsage(icommandsender))));
 			if (player == null)
-				icommandsender.addChatMessage(new ChatComponentText(LangUtils.translate("holocron.error.ship.is.null")));
+				icommandsender.addChatMessage(new ChatComponentText(String.format("[Holocron] Error! Player is null.")));
 			else if (robes == null)
-				icommandsender.addChatMessage(new ChatComponentText(LangUtils.translate("holocron.error.you.must.have.a.holocron")));
+				icommandsender.addChatMessage(new ChatComponentText(String.format("[Holocron] Error! You must have a Holocron.")));
 			else
-				icommandsender.addChatMessage(new ChatComponentText(LangUtils.translate("holocron.unknown.key")));
+				icommandsender.addChatMessage(new ChatComponentText(String.format("[Holocron] Error! Unknown key.")));
 		}
 	}
 }
