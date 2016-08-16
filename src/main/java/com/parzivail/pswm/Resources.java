a7adb211b7f099c0bc729230cd6559f176c80dea
Colby Newman <colby.newman2000@gmail.com>
2016-08-16 14:23:06 -0400

    1.3.1 actual -build

--- src/main/java/com/parzivail/pswm/Resources.java
+++ src/main/java/com/parzivail/pswm/Resources.java
@@ -156,7 +156,7 @@ public class Resources
 
 	// Core
 	public static final String MODID = "starwarsmod";
-	public static final String VERSION = "1.3.0";
+	public static final String VERSION = "1.3.1";
 	public static final String VERSION_MAJOR = "1.3";
 	public static String ONLINE_VERSION = "";
 
