9abe690b6dac970506d5aeaaa4d36ab111260673
Colby Newman <colby.newman2000@gmail.com>
2017-01-02 16:34:41 -0500

    grazz

--- src/main/java/com/parzivail/pswm/dimension/dagobah/BiomeDagobah.java
+++ src/main/java/com/parzivail/pswm/dimension/dagobah/BiomeDagobah.java
@@ -2,10 +2,12 @@ package com.parzivail.pswm.dimension.dagobah;
 
 import com.parzivail.util.Util;
 import com.parzivail.util.math.HaltonSequence;
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.feature.WorldGenTallGrass;
 import net.minecraft.world.gen.feature.WorldGenTrees;
 import net.minecraft.world.gen.feature.WorldGenWaterlily;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -20,6 +22,7 @@ public class BiomeDagobah extends Biome
 	WorldGenTrees trees = new WorldGenTrees(true, 10, Blocks.LOG.getDefaultState(), Blocks.LEAVES.getDefaultState(), true);
 	HaltonSequence haltonSequence = new HaltonSequence();
 	public WorldGenerator waterlilyGen = new WorldGenWaterlily();
+	public WorldGenerator tallgrassGen = new WorldGenTallGrass(BlockTallGrass.EnumType.GRASS);
 
 	public BiomeDagobah()
 	{
@@ -45,7 +48,7 @@ public class BiomeDagobah extends Biome
 			}
 		}
 
-		for (int k3 = 0; k3 < 4; ++k3)
+		for (int i = 0; i < 8; i++)
 		{
 			int l7 = rand.nextInt(16) + 8;
 			int k11 = rand.nextInt(16) + 8;
@@ -68,6 +71,32 @@ public class BiomeDagobah extends Biome
 				}
 
 				this.waterlilyGen.generate(worldIn, rand, blockpos4);
+			}
+		}
+
+		for (int i = 0; i < 8; i++)
+		{
+			int l7 = rand.nextInt(16) + 8;
+			int k11 = rand.nextInt(16) + 8;
+			int i15 = worldIn.getHeight(pos.add(l7, 0, k11)).getY() * 2;
+
+			if (i15 > 0)
+			{
+				int j18 = rand.nextInt(i15);
+				BlockPos blockpos4;
+				BlockPos blockpos7;
+
+				for (blockpos4 = pos.add(l7, j18, k11); blockpos4.getY() > 0; blockpos4 = blockpos7)
+				{
+					blockpos7 = blockpos4.down();
+
+					if (!worldIn.isAirBlock(blockpos7))
+					{
+						break;
+					}
+				}
+
+				this.tallgrassGen.generate(worldIn, rand, blockpos4);
 			}
 		}
 	}
