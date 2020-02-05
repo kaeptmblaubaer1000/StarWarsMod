17566f3d93592dffd6fc6d72389866234a1f9491
kb1000 <kaeptmblaubaer1000@gmail.com>
2020-02-05 13:42:08 +0100

    Make crystal ore drop lightsaber crystals (TODO: fix amount dropped with luck)

--- src/main/java/com/parzivail/pswm/blocks/BlockCrystalOre.java
+++ src/main/java/com/parzivail/pswm/blocks/BlockCrystalOre.java
@@ -1,13 +1,31 @@
 package com.parzivail.pswm.blocks;
 
+import com.parzivail.pswm.StarWarsItems;
 import com.parzivail.pswm.StarWarsMod;
+import com.parzivail.pswm.items.lightsaber.ItemLightsaberCrystal;
 import com.parzivail.util.block.BlockMultiTexture;
 import com.parzivail.util.world.HarvestLevel;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.Random;
 
 public class BlockCrystalOre extends BlockMultiTexture
 {
 	public static String[] colors = { "Black", "Blue", "Cyan", "Gray", "Green", "Pink", "Purple", "White", "Yellow", "Prism" };
+
+	public static int[] crystals = new int[colors.length];
+
+	static {
+		for (int i = 0; i < colors.length; i++) {
+			for (int j = 0; i < ItemLightsaberCrystal.colors.length; j++) {
+				if (colors[i].equalsIgnoreCase(ItemLightsaberCrystal.colors[j])) {
+					crystals[i] = j;
+				}
+			}
+		}
+	}
 
 	public BlockCrystalOre()
 	{
@@ -16,5 +34,17 @@ public class BlockCrystalOre extends BlockMultiTexture
 		setHardness(4.0F);
 		this.setHarvestLevel("pickaxe", HarvestLevel.IRON);
 		setStepSound(soundTypeMetal);
+	}
+
+	@Override
+	public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
+	{
+		return StarWarsItems.lightsaberCrystal;
+	}
+
+	@Override
+	public int damageDropped(int meta)
+	{
+		return crystals[meta];
 	}
 }
